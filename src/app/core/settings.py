156f5ade2bf14555be4c694f1aded712e68911d3
Aleksey Panin <lllrikitikilll@yandex.ru>
2024-09-30 16:36:41 +0300

    Добавлены конфигурациия Postgresql

--- src/app/core/settings.py
+++ src/app/core/settings.py
@@ -1,5 +1,19 @@
 from pydantic import BaseModel
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class PostgresConfig(BaseModel):
+    """Конфигурация для Postgres."""
+
+    user: str
+    password: str
+    host: str
+    port: str
+    db: str
+
+    @property
+    def url(self):  # noqa: W293, D102
+        return f'postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.db}'  # noqa: WPS221, WPS305, E501
 
 
 class UrlPath(BaseModel):
@@ -8,7 +22,16 @@ class UrlPath(BaseModel):
     prefix = '/api'
 
 
-class Setings(BaseSettings):
+class Settings(BaseSettings):
     """Настройки проекта."""
 
+    model_config = SettingsConfigDict(
+        case_sensitive=False,
+        env_nested_delimiter='__',
+        env_prefix='APP_CONFIG__',
+    )
+    psql: PostgresConfig
     url: UrlPath
+
+
+settings = Settings()  # type: ignore [call-arg]

0d1e7cc649b336a4ebde112fe148aa037ebc2122
Aleksey Panin <lllrikitikilll@yandex.ru>
2024-09-30 13:40:01 +0300

    Добавлены заглушки хендлеров

--- src/app/api/cat_handlers.py
+++ src/app/api/cat_handlers.py
@@ -3,3 +3,39 @@ from fastapi import APIRouter
 router = APIRouter(
     prefix='/api',
 )
+
+
+@router.post('/breeds')
+async def breeds():
+    """Получение списка пород."""
+    pass  # noqa: WPS420
+
+
+@router.post('/cats')
+async def cats():
+    """Получение списка всех котят."""
+    pass  # noqa: WPS420
+
+
+@router.post('/cats/{id}')
+async def cat_info():
+    """Получение подробной информации о котенке."""
+    pass  # noqa: WPS420
+
+
+@router.post('/cats')
+async def add_cat():
+    """Добавление информации о котенке."""
+    pass  # noqa: WPS420
+
+
+@router.patch('/cats/{id}')
+async def patch_cat():
+    """Изменение информации о котенке."""
+    pass  # noqa: WPS420
+
+
+@router.delete('/cats/{id}')
+async def delete_cat():
+    """Удалить информацию о котенке."""
+    pass  # noqa: WPS420

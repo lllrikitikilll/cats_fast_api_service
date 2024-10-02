508860ae59a75468b076f34d79de170dbd032122
Aleksey Panin <lllrikitikilll@yandex.ru>
2024-10-02 21:14:51 +0300

    Добавлены схемы: CreateCatResponse, Status, CreateCatDataModel

--- src/app/schemas/schemas.py
+++ src/app/schemas/schemas.py
@@ -1,6 +1,21 @@
+from enum import Enum
 from typing import List, Optional
 
 from pydantic import BaseModel
+
+
+class Status(Enum):
+    """Статусы ответов на запросы."""
+
+    success = 'Success'
+    error = 'Error'
+
+
+class CreateCatResponse(BaseModel):
+    """Схема ответа создания объекта Cat."""
+
+    status: Status = Status.success
+    message: str
 
 
 class BreedBase(BaseModel):
@@ -24,6 +39,15 @@ class CatBase(BaseModel):
 
     class Config:  # noqa: D106
         from_attributes = True
+
+
+class CreateCatDataModel(BaseModel):
+    """Схема данных на создание объекта кошачих."""
+
+    color: str
+    age_in_months: int
+    description: Optional[str] = None
+    breed_id: int
 
 
 class CatListResponseModel(BaseModel):

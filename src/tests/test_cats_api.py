37f9401af3a85d1c2ec7cd26e97ae6beab7d95c4
Aleksey Panin <lllrikitikilll@yandex.ru>
2024-10-02 19:47:29 +0300

    Добавлен тест test_get_cats_with_breed

--- src/tests/test_cats_api.py
+++ src/tests/test_cats_api.py
@@ -15,7 +15,6 @@ async def test_get_all_cats(test_client, cat_payload, breed_payload):
     assert response_json["cats"][0]["description"] == cat_payload["description"]
 
 
-@pytest.mark.asyncio
 @pytest.mark.api
 @pytest.mark.integration
 async def test_get_all_breeds(test_client, breed_payload):
@@ -25,3 +24,14 @@ async def test_get_all_breeds(test_client, breed_payload):
 
     assert response.status_code == status.HTTP_200_OK
     assert response_json["breeds"][0]["name"] == breed_payload["name"]
+
+
+@pytest.mark.api
+@pytest.mark.integration
+async def test_get_cats_with_breed(test_client, breed_payload, breed_name):
+    """Тест взятия всех котов определенной породы из БД."""
+    response = await test_client.post(f"/api/cats/breeds/{breed_name}")
+    response_json = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response_json["cats"][0]["breed"]["name"] == breed_payload["name"]

25d0c4b3534bf8684dc1a8d118cd7a0ebcb5f489
Aleksey Panin <lllrikitikilll@yandex.ru>
2024-10-02 19:46:27 +0300

    Добавлен метод возврата списка кошек опред породы get_cats_with_breed

--- src/app/service/cat.py
+++ src/app/service/cat.py
@@ -71,6 +71,37 @@ class CatService:
             )
         return breeds
 
+    async def get_cats_with_breed(self, session: AsyncSession, breed: str):
+        """Запрос всех кошек заданной породы.
+
+        Args:
+            session (AsyncSession): асинхронная сессия
+            breed (str): название породы
+
+        Raises:
+            HTTPException: Ошибка 404 если породы нет
+
+        Returns:
+            list[Cat]: Список кошек заданной породы
+        """
+        stmt = select(Cat).options(selectinload(Cat.breed)).join(Breed).where(Breed.name == breed)  # noqa: E501, WPS221
+        result_db: Result = await session.execute(statement=stmt)
+
+        try:
+            cats = list(result_db.scalars().all())
+        except SQLAlchemyError as exp:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Возникла ошибка при получении списка кошек с такой породой.",
+            ) from exp
+
+        if not cats:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='Порода отсутствуют.',
+            )
+        return cats
+
 
 cat_service = CatService()
 

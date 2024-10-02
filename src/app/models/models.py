e30352807cc7573933842014bb3552370ae47692
Aleksey Panin <lllrikitikilll@yandex.ru>
2024-10-02 23:38:56 +0300

    Убрана валидация

--- src/app/models/models.py
+++ src/app/models/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import ForeignKey, String
-from sqlalchemy.orm import Mapped, mapped_column, relationship, validates
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.app.models.base import Base
 
@@ -14,20 +14,6 @@ class Cat(Base):
     description: Mapped[str] = mapped_column(String(), nullable=True)
     breed_id: Mapped[int] = mapped_column(ForeignKey('breed.id'))
     breed: Mapped['Breed'] = relationship(back_populates='cats')
-
-    @validates('age_in_months')
-    def validate_amount_gt_zero(self, key, age_in_months) -> int:
-        """
-        Валидация возраста кота. Проверяет, что возраст больше нуля.
-
-        :param int age_in_months: Возраст кота в месяцах.
-        :raises ValueError: Если возраст меньше или равен нулю.
-        :return: Возраст кота в месяцах, если он валиден.
-        :rtype: int
-        """
-        if age_in_months <= 0:
-            raise ValueError('Возраст должен быть больше нуля')
-        return age_in_months
 
 
 class Breed(Base):

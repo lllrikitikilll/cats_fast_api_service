15db973cd148e55e24d8b75877f834ea6e3c275d
Aleksey Panin <lllrikitikilll@yandex.ru>
2024-10-02 17:10:55 +0300

    Изменен способ получения сессии

--- src/app/models/db_helper.py
+++ src/app/models/db_helper.py
@@ -1,33 +1,19 @@
-from typing import AsyncGenerator
-
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from src.app.core.settings import settings
 
+engine = create_async_engine(url=settings.psql.url)
 
-class DatabaseHelper:
-    """Управление подключением к БД."""
-
-    def __init__(self, uri: str, echo: bool = False):
-        self.engine = create_async_engine(
-            url=uri,
-            echo=echo,
-        )
-        self.session_factory = async_sessionmaker(
-            bind=self.engine,
-            autoflush=False,
-            autocommit=False,
-            expire_on_commit=False,
-        )
-
-    async def session_dependency(self) -> AsyncGenerator:
-        """Создание генератора сессий для DI."""
-        async with self.session_factory() as session:
-            yield session
-            await session.close()
-
-
-db_helper = DatabaseHelper(
-    uri=settings.psql.url,
-    echo=False,
+session_factory = async_sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+    expire_on_commit=False,
 )
+
+
+async def get_db():
+    """Создание генератора сессий."""
+    async with session_factory() as session:
+        yield session
+        await session.close()

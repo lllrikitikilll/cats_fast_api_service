a7a41e503c2a22ad202e3f5bd98eb0e28fd0be97
Aleksey Panin <lllrikitikilll@yandex.ru>
2024-10-02 17:11:27 +0300

    Изменен способ получения сессии

--- src/app/api/cat_handlers.py
+++ src/app/api/cat_handlers.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.app.core.settings import settings
-from src.app.models.db_helper import db_helper
+from src.app.models.db_helper import get_db
 from src.app.schemas import schemas
 from src.app.service.cat import CatService, get_cat_service
 
@@ -11,9 +11,9 @@ router = APIRouter(
 )
 
 
-@router.post('/cats')
+@router.post("/cats")
 async def get_all_cats(
-    session: AsyncSession = Depends(db_helper.session_dependency),
+    session: AsyncSession = Depends(get_db),
     cat_service: CatService = Depends(get_cat_service),
 ) -> schemas.CatListResponseModel:
     """Получение списка всех котят."""
@@ -21,9 +21,9 @@ async def get_all_cats(
     return schemas.CatListResponseModel(cats=cats)
 
 
-@router.post('/cats/breeds')
+@router.post("/cats/breeds")
 async def all_breeds(
-    session: AsyncSession = Depends(db_helper.session_dependency),
+    session: AsyncSession = Depends(get_db),
     cat_service: CatService = Depends(get_cat_service),
 ) -> schemas.BreedListResponseModel:
     """Получение списка пород."""
@@ -31,31 +31,31 @@ async def all_breeds(
     return schemas.BreedListResponseModel(breeds=breeds)
 
 
-@router.post('/cats/breeds/{breed}')
+@router.post("/cats/breeds/{breed}")
 async def cats_with_breeds(breed: str):
     """Получение списка пород."""
     pass  # noqa: WPS420
 
 
-@router.post('/cats/{cat_id}')
+@router.post("/cats/{cat_id}")
 async def cat_info(cat_id: int):
     """Получение подробной информации о котенке."""
     pass  # noqa: WPS420
 
 
-@router.post('/cats')
+@router.post("/cats")
 async def add_cat():
     """Добавление информации о котенке."""
     pass  # noqa: WPS420
 
 
-@router.patch('/cats/{cat_id}')
+@router.patch("/cats/{cat_id}")
 async def patch_cat(cat_id: int):
     """Изменение информации о котенке."""
     pass  # noqa: WPS420
 
 
-@router.delete('/cats/{cat_id}')
+@router.delete("/cats/{cat_id}")
 async def delete_cat(cat_id: int):
     """Удалить информацию о котенке."""
     pass  # noqa: WPS420
